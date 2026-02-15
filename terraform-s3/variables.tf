9842782db7a7ca5a1fb1c8511fb58e06c7ca3cb7
Manish Karki <90179912+manishkarki148@users.noreply.github.com>
2026-02-15 14:41:13 +1000

    Add aws_region variable to variables.tf
    Added aws_region variable with default value.

--- terraform-s3/variables.tf
+++ terraform-s3/variables.tf
@@ -8,3 +8,10 @@ variable "bucket_name" {
   description = "Name of the S3 bucket"
   type        = string
 }
+
+variable "aws_region" {
+  description = "AWS region to deploy resources"
+  type        = string
+  default     = "us-east-1"  # optional default
+}
+
