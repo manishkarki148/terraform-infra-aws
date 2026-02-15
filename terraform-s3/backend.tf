c94a95db463f22bc1d5470269aff700f9595703d
Manish Karki <90179912+manishkarki148@users.noreply.github.com>
2026-02-15 14:37:14 +1000

    Remove region from S3 backend configuration
    Removed region configuration from S3 backend.

--- terraform-s3/backend.tf
+++ terraform-s3/backend.tf
@@ -4,7 +4,6 @@ terraform {
   backend "s3" {
     bucket = "mk-terraform-backend-s3"   # Replace with your S3 bucket
     key    = "envs/dev/s3-1/terraform.tfstate" # Path to state file in the bucket
-    region = var.aws_region
 
     # Enable server-side encryption
     encrypt = true
