4678fc13229e42014868e277a747f6099b68b2f9
Manish Karki <90179912+manishkarki148@users.noreply.github.com>
2026-02-15 14:38:55 +1000

    Modify Terraform and AWS provider version constraints
    Updated required Terraform version and AWS provider version.

--- terraform-s3/providers.tf
+++ terraform-s3/providers.tf
@@ -1,13 +1,12 @@
 terraform {
-  required_version = ">= 1.3.0"
-
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 5.0.0, < 6.0.0"
+      version = ">= 6.28.0, < 7.0.0"
     }
   }
 }
+
 
 provider "aws" {
   region = var.aws_region
