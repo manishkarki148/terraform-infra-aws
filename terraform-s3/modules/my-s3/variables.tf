2adbd725204e464cfbab4f34188408903c83cd33
Manish Karki <90179912+manishkarki148@users.noreply.github.com>
2026-02-21 18:40:00 +1000

    Define new variables for S3 module
    Add variables for AWS region and account role ARN

--- terraform-s3/modules/my-s3/variables.tf
+++ terraform-s3/modules/my-s3/variables.tf
@@ -2,3 +2,13 @@ variable "bucket_name" {
   description = "Name of the S3 bucket"
   type        = string
 }
+
+variable "aws_region" {
+  description = "AWS region to deploy resources"
+  type        = string
+  default     = "us-east-1"  # optional default
+}
+
+variable "account_b_role_arn" {
+  type = string
+}
