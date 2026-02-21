5b7c9cf002619dd6ad287e49659db9d7b63f9ff9
Manish Karki <90179912+manishkarki148@users.noreply.github.com>
2026-02-21 20:52:10 +1000

    Update main.tf

--- terraform-s3-account-b/modules/my-s3/main.tf
+++ terraform-s3-account-b/modules/my-s3/main.tf
@@ -1,4 +1,4 @@
-
+/*
 terraform {
 
   required_version = ">= 1.3.0"
@@ -25,7 +25,7 @@ provider "aws" {
     role_arn = "arn:aws:iam::376408658479:role/MK-Github-Pipeline-Access"
   }
 }
-
+*/
 
 module "vendor_s3" {
 /*
