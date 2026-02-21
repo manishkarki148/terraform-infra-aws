38d6cd8b1fd9db3188d982943ca5acc974ec1e74
Manish Karki <90179912+manishkarki148@users.noreply.github.com>
2026-02-21 17:34:26 +1000

    Update S3 backend configuration in backend.tf
    Changed the S3 backend region to 'us-east-1' and commented out the use_lockfile option.

--- terraform-s3/backend.tf
+++ terraform-s3/backend.tf
@@ -3,7 +3,7 @@ terraform {
     bucket = "mk-terraform-backend-s3"   # Replace with your S3 bucket
     key    = "envs/dev/s3-1/terraform.tfstate" # Path to state file in the bucket
     encrypt = true
-    region = var.aws_region
-    use_lockfile = true
+    region = us-east-1
+    #use_lockfile = true
   }
 }
