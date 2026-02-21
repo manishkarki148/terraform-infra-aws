6ecbf25114f01f96b264772495d090cbe70e82c8
Manish Karki <90179912+manishkarki148@users.noreply.github.com>
2026-02-21 17:53:22 +1000

    Remove assume_role and region from AWS provider
    Removed AWS provider configuration for account B.

--- terraform-s3/main.tf
+++ terraform-s3/main.tf
@@ -3,11 +3,6 @@
 module "mk_s3_bucket" {
   provider "aws" {
     alias  = "account_b"
-    region = var.aws_region
-  
-    assume_role {
-      role_arn = var.account_b_role_arn
-    }
   }
   source = "./modules/my-s3"
   bucket_name = var.bucket_name
