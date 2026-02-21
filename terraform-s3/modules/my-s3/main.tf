eca9095145b15c6c770adbbd539f81628b6fbf24
Manish Karki <90179912+manishkarki148@users.noreply.github.com>
2026-02-21 18:08:18 +1000

    Configure Terraform with AWS provider settings
    Add required providers and AWS provider configurations.

--- terraform-s3/modules/my-s3/main.tf
+++ terraform-s3/modules/my-s3/main.tf
@@ -1,3 +1,29 @@
+terraform {
+
+  required_version = ">= 1.3.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 6.28.0, < 7.0.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+provider "aws" {
+  alias  = "account_b"
+  region = var.aws_region
+
+  assume_role {
+    role_arn = var.account_b_role_arn
+  }
+}
+
+
 module "vendor_s3" {
   source  = "terraform-aws-modules/s3-bucket/aws"
   version = "5.10.0"
