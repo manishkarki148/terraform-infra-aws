de0d546a6079970ac1fc353f67364c347dcca955
Manish Karki <90179912+manishkarki148@users.noreply.github.com>
2026-03-01 16:03:59 +1000

    Update main.tf

--- terraform-s3-account-b/main.tf
+++ terraform-s3-account-b/main.tf
@@ -1,4 +1,13 @@
 
+module "mk_s3_bucket_a" {
+  providers = {
+    aws = aws.account_a
+  }
+  source = "./modules/my-s3"
+  bucket_name = var.bucket_name_a
+  account_a_role_arn = var.account_a_role_arn
+}
+
 module "mk_s3_bucket_b" {
   providers = {
     aws = aws.account_b
@@ -8,11 +17,4 @@ module "mk_s3_bucket_b" {
   account_b_role_arn = var.account_b_role_arn
 }
 
-module "mk_s3_bucket_a" {
-  providers = {
-    aws = aws.account_a
-  }
-  source = "./modules/my-s3"
-  bucket_name = var.bucket_name_a
-  account_a_role_arn = var.account_a_role_arn
-}
+
