35f9d18f47dd25a017b6860f9796acd9297a4df7
Manish Karki <90179912+manishkarki148@users.noreply.github.com>
2026-03-01 15:35:32 +1000

    Update backend.tf

--- terraform-s3-account-b/backend.tf
+++ terraform-s3-account-b/backend.tf
@@ -4,7 +4,7 @@ terraform {
     key    = "envs/dev/s3-2/terraform.tfstate" # Path to state file in the bucket
     encrypt = true
     region = "us-east-1"
-    profile = "default"
+    #profile = "default"
     use_lockfile = true
   }
 }
